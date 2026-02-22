1fc15a345abd98e9337e9fae358d7ddb0e0e6f4b
k22rs045 <k22rs045@st.kyusan-u.ac.jp>
2026-02-22 12:02:56 +0900

    エンディングの配置をした

--- Assets/All/Scripts/EndingScript.cs
+++ Assets/All/Scripts/EndingScript.cs
@@ -1,18 +1,61 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EndingScript : MonoBehaviour
 {
     [SerializeField] Text text;
+
+    [System.Serializable]
+    public class ParentGroup
+    {
+        public Transform parent;
+        public int selectCount;
+        public Sprite sprite1;
+        public Sprite sprite2;
+    }
+
+    public ParentGroup parentA;
+    public ParentGroup parentB;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        parentA.selectCount = GManager.Instance.seniorHealthScore;
+        parentB.selectCount = GManager.Instance.juniorHealthScore;
+
+        SelectFromParent(parentA);
+        SelectFromParent(parentB);
     }
 
-    // Update is called once per frame
-    void Update()
+    void SelectFromParent(ParentGroup group)
     {
-        
+        List<Image> images = new List<Image>();
+
+        foreach (Transform child in group.parent)
+        {
+            Image img = child.GetComponent<Image>();
+            if (img != null)
+                images.Add(img);
+        }
+
+        int max = images.Count;
+        int count = Mathf.Min(group.selectCount, max);
+
+        // シャッフル
+        for (int i = 0; i < images.Count; i++)
+        {
+            int rand = Random.Range(i, images.Count);
+            (images[i], images[rand]) = (images[rand], images[i]);
+        }
+
+        // 選ばれたものに2種類ランダムで割り当て
+        for (int i = 0; i < count; i++)
+        {
+            if (Random.value < 0.5f)
+                images[i].sprite = group.sprite1;
+            else
+                images[i].sprite = group.sprite2;
+        }
     }
 }
